ec9a33d0932254bc96f9ce8c7b7458f58a02fd44
Risto Koverola <risto.koverola@eficode.com>
2022-06-07 18:22:34 +0300

    Solve exercise 1.5

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -7,40 +7,42 @@ const Content = (props) => {
   console.log(props)
   return (
     <>
-      <p>{props.part1.name}{props.part1.exercises}</p>
-      <p>{props.part2.name}{props.part2.exercises}</p>
-      <p>{props.part3.name}{props.part3.exercises}</p>
+      <p>{props.parts[0].name}{props.parts[0].exercises}</p>
+      <p>{props.parts[1].name}{props.parts[1].exercises}</p>
+      <p>{props.parts[2].name}{props.parts[2].exercises}</p>
     </>
   )
 }
 const Total = (props) => {
   return (
     <>
-      <p>Number of exercises {props.part1.exercises + props.part2.exercises + props.part3.exercises}</p>
+      <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
     </>
   )
 }
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} part2={part2} part3={part3} />
-      <Total part1={part1} part2={part2} part3={part3} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   )
 }
