46e49a313781db13d6de18b53f26dc7bc300cf10
Risto Koverola <risto.koverola@eficode.com>
2022-06-08 14:14:30 +0300

    Solve 1.11

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -6,7 +6,16 @@ const Button = (props) => {
   )
 }
 
-const StatisticLine = (props) => <div>{props.text} {props.value}</div>
+const StatisticLine = (props) => {
+  return (
+    <tbody>
+      <tr>
+        <td>{props.text}</td>
+        <td>{props.value}</td>
+      </tr>
+    </tbody>
+  )
+}
 
 const Statistics = (props) => {
   if(props.good + props.neutral + props.bad === 0) {
@@ -15,13 +24,13 @@ const Statistics = (props) => {
     )
   }
   return (
-    <div>
+    <table>
       <StatisticLine text={'good'} value={props.good} />
       <StatisticLine text={'neutral'} value={props.neutral} />
       <StatisticLine text={'bad'} value={props.bad} />
       <StatisticLine text={'average'} value={props.average} />
       <StatisticLine text={'positive'} value={props.positive} />
-    </div>
+    </table>
   )
 }
 
@@ -32,10 +41,12 @@ const App = () => {
   const [bad, setBad] = useState(0)
 
   const calculateAll = () => good + neutral + bad
-
-  // FIXME: Divide by zero?
   const calculateAverage = () => ((1 * good + 0 * neutral + -1* bad) / calculateAll())
-  const calculatePositivePrecentage = () => good / calculateAll()
+  const calculatePositivePercentage = () => {
+    const positive = good / calculateAll()
+    const percentage = (100 * positive).toPrecision(3) + ' %'
+    return percentage
+  } 
 
 
   return (
@@ -45,7 +56,7 @@ const App = () => {
       <Button handler={() => setNeutral(neutral + 1)} text={'neutral'}/>
       <Button handler={() => setBad(bad + 1)} text={'bad'}/>
       <h1>statistics</h1>
-      <Statistics good={good} neutral={neutral} bad={bad} average={calculateAverage()} positive={calculatePositivePrecentage()}/>
+      <Statistics good={good} neutral={neutral} bad={bad} average={calculateAverage()} positive={calculatePositivePercentage()}/>
     </div>
   )
 }
