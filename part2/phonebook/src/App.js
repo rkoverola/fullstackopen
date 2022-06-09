2df76b5d730b8798bf222c1d3c742b0965fb4b20
Risto Koverola <risto.koverola@eficode.com>
2022-06-09 16:17:30 +0300

    Solve 2.6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,24 +1,46 @@
 import { useState } from 'react'
 
+const PersonList = ({persons}) => {
+  return (
+    <div>
+      {persons.map(p => <div key={p.name}>{p.name}</div>)}
+    </div>
+  )
+}
+
 const App = () => {
+
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas' }
   ]) 
   const [newName, setNewName] = useState('')
+
+  const handleNewNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+  
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName
+    }
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
       <form>
         <div>
-          name: <input />
+          Name: <input onChange={handleNewNameChange} value={newName}/>
         </div>
         <div>
-          <button type="submit">add</button>
+          <button type="submit" onClick={addPerson}>Add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <PersonList persons={persons}/>
     </div>
   )
 }
