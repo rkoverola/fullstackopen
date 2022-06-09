488a680a38917f973be829bf997394c2866d62f2
Risto Koverola <risto.koverola@eficode.com>
2022-06-09 19:30:45 +0300

    Solve 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const FilterWidget = ({handler, filterString}) => {
   return (
@@ -40,15 +41,20 @@ const PersonList = ({persons, filterString}) => {
 
 const App = () => {
 
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filterString, setFilterString] = useState('')
+
+  useEffect(() => {
+    console.log('Using effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then((response) => {
+        console.log('Data from server ->', response.data)
+        setPersons(response.data)
+      })
+  }, [])
 
   const handleNewNameChange = (event) => setNewName(event.target.value)
   const handleNewNumberChange = (event) => setNewNumber(event.target.value)
