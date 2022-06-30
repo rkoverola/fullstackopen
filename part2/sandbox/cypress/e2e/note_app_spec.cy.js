2d3ef284e838e2e5cce06614174a6780db0f145b
Risto Koverola <risto.koverola@eficode.com>
2022-06-30 11:01:42 +0300

    Save progress until 5.17

--- part2/sandbox/cypress/e2e/note_app_spec.cy.js
+++ part2/sandbox/cypress/e2e/note_app_spec.cy.js
@@ -32,17 +32,25 @@ describe('Note app', function() {
       cy.get('#login-button').click()
       cy.contains('Matti Luukkainen logged in')
     })
+
+    it('login fails when password is incorrect', function() {
+      cy.contains('Log in').click()
+      cy.get('#username').type('mluukkai')
+      cy.get('#password').type('wrong password')
+      cy.get('#login-button').click()
+      cy.get('.error')
+        .should('contain', 'Wrong username or password')
+        .and('have.css', 'color', 'rgb(255, 0, 0)')
+        .and('have.css', 'border-style', 'solid')
+
+      cy.get('html').should('not.contain', 'Matti Luukkainen logged in')
+    })
   })
 
   describe('When logged in', function() {
 
     beforeEach(function() {
-      cy.visit('http://localhost:3000')
-      cy.contains('Log in').click()
-      cy.get('#username').type('mluukkai')
-      cy.get('#password').type('salainen')
-      cy.get('#login-button').click()
-      cy.contains('Matti Luukkainen logged in')
+      cy.login({ username: 'mluukkai', password: 'salainen' })
     })
 
     it('a new note can be created', function() {
@@ -54,18 +62,33 @@ describe('Note app', function() {
 
     describe('and a note exists', function () {
       beforeEach(function () {
-        cy.contains('Create new').click()
-        cy.get('input:last').type('another note cypress')
-        cy.contains('Save').click()
+        cy.createNote({ content: 'note from cypress', important: false })
       })
 
       it('it can be made important', function () {
-        cy.contains('another note cypress')
-          .contains('Make important')
+        cy.contains('note from cypress')
+          .parent()
+          .find('button')
           .click()
 
-        cy.contains('another note cypress')
-          .contains('Make not important')
+        cy.contains('note from cypress')
+          .parent()
+          .should('contain', 'Make not important')
+      })
+    })
+
+    describe('and several notes exist', function () {
+      beforeEach(function () {
+        cy.createNote({ content: 'first note', important: false })
+        cy.createNote({ content: 'second note', important: false })
+        cy.createNote({ content: 'third note', important: false })
+      })
+
+      it('one of those can be made important', function () {
+        cy.contains('second note').parent().find('button').click()
+
+        cy.contains('second note').parent().find('button')
+          .should('contain', 'Make not important')
       })
     })
   })
--- part2/sandbox/src/App.js
+++ part2/sandbox/src/App.js
@@ -115,7 +115,7 @@ const App = () => {
   }
 
   const loggedIn = () => {
-    const message = user ? `${user.name} logged in` : 'sorry nothing'
+    const message = user ? `${user.name} logged in` : ''
     return (
       <div>{message}</div>
     )
